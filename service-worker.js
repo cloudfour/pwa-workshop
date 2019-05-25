d8d9b54cf964d31bee5e0a61622c5c3df216a321
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2019-05-25 13:29:03 -0700

    Finish basic `install` service worker step

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,33 @@
 /**
- * If you need to force pages to create a fresh cache, increment 
- * the `CACHE_VERSION` number. Doing so will kick of the service
- * worker update flow allowing old cache(s) to be deleted during the
- * `activate` event handler.
+ * This sets up a two-level version cache names.
+ * Edit the GLOBAL_VERSION to purge all caches.
+ * Edit the individual cache version to purge specific cache.
  */
-const CACHE_VERSION = 1;
-const CACHE_NAME = `my-site-cache-v${CACHE_VERSION}`;
+// const GLOBAL_VERSION = 1;
+// const CACHES = {
+//   PRECACHE: `old-navy-pwa-precache-v${GLOBAL_VERSION + 0}`,
+//   NUXT: `old-navy-pwa-assets-nuxt-v${GLOBAL_VERSION + 1}`,
+//   GAP: `old-navy-pwa-assets-gap-v${GLOBAL_VERSION + 0}`,
+//   PROLIFIC: `old-navy-pwa-assets-prolific-v${GLOBAL_VERSION + 0}`
+// };
 
-const MUST_HAVE_URLS = [
-  '/',
+/**
+ * This sets up a two-level version cache system.
+ * Edit the `GLOBAL_VERSION` to delete all caches.
+ * Edit the individual cache version to delete a specific cache.
+ */
+const GLOBAL_VERSION = 1;
+const STATIC_ASSETS_CACHE_NAME = `pwa-workshop-static-assets-v${GLOBAL_VERSION + 0}`;
+const RUNTIME_CACHE_NAME = `pwa-workshop-runtime-v${GLOBAL_VERSION + 0}`;
+
+const MUST_HAVE_STATIC_ASSETS = [
   '/css/styles.css',
-  '/js/register-service-worker.js',
-  'images/sky-friendly-robot.svg'
+  '/images/sky-friendly-robot.svg',
+  '/fonts/source-sans-pro-v9-latin_latin-ext-regular.woff2',
+  '/fonts/source-code-pro-v6-latin-regular.woff2',
+  '/scripts/main.js'
 ];
-const NICE_TO_HAVE_URLS = [
+const NICE_TO_HAVE_STATIC_ASSETS = [
   'images/portland.svg'
 ];
 
@@ -21,18 +35,18 @@ const NICE_TO_HAVE_URLS = [
  * Ideal time to cache CSS, JS, images, fonts static assets
  */
 self.addEventListener('install', event => {
-  console.log('Installing');
+  console.log('Installing...');
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(STATIC_ASSETS_CACHE_NAME)
       .then(cache => {
         /**
          * If these assets fail to cache, it's okay
          */
-        cache.addAll(NICE_TO_HAVE_URLS);
+        cache.addAll(NICE_TO_HAVE_STATIC_ASSETS);
         /**
          * These assets are required 
          */
-        return cache.addAll(MUST_HAVE_URLS);
+        return cache.addAll(MUST_HAVE_STATIC_ASSETS);
       })
       .catch(error => {
         console.log('Install failed:', error);
