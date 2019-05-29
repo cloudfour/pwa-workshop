6453c7179327b5387227eebdcf623ead50284e7e
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2019-05-29 14:19:45 -0700

    Feature: Refactor to add staleWhileRevalidate function + custom logging styles (#11)

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,7 @@
 const GLOBAL_VERSION = 1;
 const PWA_WORKSHOP = 'pwa-workshop';
 const CACHES = {
+  PWA_WORKSHOP: `${PWA_WORKSHOP}-v${GLOBAL_VERSION + 0}`,
   PRECACHED_ASSETS: `${PWA_WORKSHOP}-precached-assets-v${GLOBAL_VERSION + 0}`,
   IMAGES: `${PWA_WORKSHOP}-images-v${GLOBAL_VERSION + 0}`,
   RUNTIME: `${PWA_WORKSHOP}-runtime-v${GLOBAL_VERSION + 0}`
@@ -49,7 +50,7 @@ self.addEventListener('install', event => {
 
   event.waitUntil(async function() {
     // Open the cache.
-    const cache = await caches.open(CACHES.PRECACHED_ASSETS);
+    const cache = await caches.open(CACHES.PWA_WORKSHOP);
 
     // Install will not stop if one of these assets fails to cache.
     cache.addAll(PRECACHE_ASSETS.NICE_TO_HAVE)
@@ -102,6 +103,69 @@ self.addEventListener('activate', event => {
 const isImageRequest = request => IMAGE_REGEX.test(request.url);
 
 /**
+ * Helper to provide a custom styled console.info message
+ * 
+ * @param {Object} obj
+ * @param {string} obj.heading The heading (title) to log
+ * @param {string} obj.fetchType The message fetch type (Cache/Network)
+ * @param {string} obj.url The URL of the fetch
+ */
+const logInfo = ({ heading, fetchType, url }) => {
+  const headingStyles = [
+    'font-weight: bold',
+    'background: #456bd9',
+    'border: 1px solid #456bd9',
+    'color: white',
+    'padding: .6em'
+  ].join(';');
+  const fetchTypeStyles = [
+    'font-weight: normal',
+    'border: 1px dotted gray',
+    'padding: .6em'
+  ].join(';');
+  const urlStyles = [
+    'font-weight: normal',
+    'padding: .6em'
+  ].join(';');
+
+  console.info(
+    `%c${heading}%cFrom ${fetchType}%c${url}`, 
+    headingStyles, 
+    fetchTypeStyles, 
+    urlStyles
+  );
+}
+
+/**
+ * Helper for the Stale-While-Revalidate caching strategy
+ * 
+ * @see https://jakearchibald.com/2014/offline-cookbook/#stale-while-revalidate
+ * 
+ * @param {FetchEvent} event A fetch event object
+ * @returns {Promise} Resolves to a fetch Response object
+ */
+const staleWhileRevalidate = async event => {
+  const { request } = event;
+
+  const cache = await caches.open(CACHES.PWA_WORKSHOP);
+  const cachedResponse = await cache.match(request);
+  const networkResponsePromise = fetch(request);
+
+  event.waitUntil(async function() {
+    const networkResponse = await networkResponsePromise;
+    cache.put(request, networkResponse.clone())
+  }());
+
+  if (cachedResponse) {
+    logInfo({ heading: 'Stale-While-Revalidate', fetchType: 'Cache', url: request.url });
+    return cachedResponse;
+  }
+
+  logInfo({ heading: 'Stale-While-Revalidate', fetchType: 'Network', url: request.url });
+  return networkResponsePromise;
+};
+
+/**
  * Handle the `fetch` service worker event.
  */
 self.addEventListener('fetch', event => {
@@ -111,27 +175,13 @@ self.addEventListener('fetch', event => {
   // Local URLs
   if (requestURL.origin === location.origin) {
     /**
-     * Images
-     * Stale-While-Revalidate
+     * Handle images
      */
     if (isImageRequest(request)) {
-      event.respondWith(async function() {
-        const cachedImage = await caches.match(request);
-        if (cachedImage) {
-          console.info('Cache Fetch:', request.url);
-          return cachedImage;
-        }
-        
-        const networkImage = await fetch(request);
-        const imagesCache = await caches.open(CACHES.IMAGES);
-        event.waitUntil(
-          imagesCache.put(request, networkImage.clone())
-        );
-
-        console.info('Network Fetch:', request.url);
-        return networkImage;
-      }());
-      // Exit 
+      event.respondWith(
+        staleWhileRevalidate(event)
+      );
+      // Exit, no need to continue `fetch` handler logic
       return;
     }
   }
@@ -144,11 +194,13 @@ self.addEventListener('fetch', event => {
    */
   event.respondWith(async function() {
     const cachedResponse = await caches.match(request);
+
     if (cachedResponse) {
-      console.info('Cache Fetch:', request.url);
+      logInfo({ heading: 'Fallback', fetchType: 'Cache', url: request.url });
       return cachedResponse;
     }
-    console.info('Network Fetch:', request.url);
+
+    logInfo({ heading: 'Fallback', fetchType: 'Network', url: request.url });
     return fetch(request);
 }());
 });
